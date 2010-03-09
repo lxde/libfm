94a50769f249eaca1d71177223a79225a26e7efd
Hong Jen Yee (PCMan) <pcman.tw@gmail.com>
2010-03-09 22:44:57 +0800

    Add i18n support to libfm-pref-apps.

--- src/tools/libfm-pref-apps.c
+++ src/tools/libfm-pref-apps.c
@@ -139,6 +139,12 @@ int main(int argc, char** argv)
     GAppInfo* app;
     char* cmd;
 
+#ifdef ENABLE_NLS
+    bindtextdomain(GETTEXT_PACKAGE, PACKAGE_LOCALE_DIR);
+    bind_textdomain_codeset(GETTEXT_PACKAGE, "UTF-8");
+    textdomain( GETTEXT_PACKAGE );
+#endif
+
     gtk_init(&argc, &argv);
     fm_gtk_init(NULL);
 
