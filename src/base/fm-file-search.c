13ceca2ef54e2030b38d4e0a4702281056b62d69
Shae Smittle <starfall87@gmail.com>
2010-06-16 03:01:12 -0500

    added basic finalize function

--- src/base/fm-file-search.c
+++ src/base/fm-file-search.c
@@ -1,5 +1,8 @@
 #include "fm-file-search.h"
+
 #include "fm-folder.h"
+
+static void fm_file_search_finalize(GObject * object)
 
 G_DEFINE_TYPE(FmFileSearch, fm_file_search, FM_TYPE_FOLDER);
 
@@ -7,8 +10,7 @@ static void fm_file_search_class_init(FmFileSearchClass *klass)
 {
 	GObjectClass *g_object_class;
     g_object_class = G_OBJECT_CLASS(klass);
-	/* TODO: Override the finalize function here */
-    /* g_object_class->finalize = fm_file_search_finalize; */
+    g_object_class->finalize = fm_file_search_finalize;
     fm_file_search_parent_class = (GObjectClass*)g_type_class_peek(FM_TYPE_FOLDER);
 
 	/* TODO: Set up signals here if needed */
@@ -22,46 +24,21 @@ static void fm_file_search_init(FmFileSearch *self)
 	self->check_type = FALSE;
 }
 
+static void fm_file_search_finalize(GObject * object)
+{
+	FmFileSearch *self;
+    g_return_if_fail(object != NULL);
+    g_return_if_fail(FM_IS_FILE_SEARCH(object));
+
+    self = FM_FILE_SEARCH(object);
+
+	/* TODO: Finalize FmFileSearch here */
+
+	if (G_OBJECT_CLASS(fm_file_search_parent_class)->finalize)
+        (* G_OBJECT_CLASS(fm_file_search_parent_class)->finalize)(object);
+}
+
 FmFileSearch * fm_file_search_new_internal()
 {
 	FmFileSearch * file_search = FM_FILE_SEARCH(g_object_new(FM_TYPE_FILE_SEARCH, NULL));
-	FmFolder * folder = FM_FOLDER(file_search);
-}
-
-
-/* util functions */
-
-/* checks to see if the file "file" is a match to "target" and "type" */
-static gboolean check_file_match(FmFileInfo * file, char * target, FmMimeType * type)
-{
-	if(g_strcmp0(fm_mime_type_get_type(fm_file_info_get_mime_type(file)),fm_mime_type_get_type(type)) == 0)
-	{
-		if(g_ascii_strcasecmp(fm_file_info_get_disp_name(file),target) == 0)
-			return TRUE;
-
-		//TODO: check contents of files of plain text file types
-	}
-
-	return FALSE;
-}
-
-static void search_folder_helper_foreach()
-{
-
-}
-
-/* recursively searches a folder using check_file_match */
-static void search_folder_helper(FmFileSearch * search, FmFolder * folder)
-{
-	FmFileInfoList * files = fm_folder_get_files(folder);
-
-	if(files != NULL)
-	{
-		//fm_list_foreach();
-	}
-}
-
-static void search_folder(FmFileSearch * search)
-{
-
 }
