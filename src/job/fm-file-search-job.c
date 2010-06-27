33476d7fbb671753acfa979bdf26b90cde002b24
Shae Smittle <starfall87@gmail.com>
2010-06-27 18:19:01 -0500

    work on file search job

---
+++ src/job/fm-file-search-job.c
@@ -0,0 +1,113 @@
+#include "fm-file-search-job.h"
+
+#include "fm-folder.h"
+#include "fm-list.h"
+#include <string.h> /* for strstr */
+
+static void fm_file_search_job_finalize(GObject * object);
+gboolean fm_file_search_job_run(FmFileSearchJob* job);
+
+G_DEFINE_TYPE(FmFileSearchJob, fm_file_search_job, FM_TYPE_JOB);
+
+static void for_each_target_folder(gpointer data, gpointer user_data);
+static void on_target_folder_loaded(FmFolder * folder, gpointer data);
+static void for_each_file_info(gpointer data, gpointer user_data);
+
+static void fm_file_search_job_class_init(FmFileSearchJobClass * klass)
+{
+	GObjectClass * g_object_class;
+	FmJobClass * job_class = FM_JOB_CLASS(klass);
+	g_object_class = G_OBJECT_CLASS(klass);
+	g_object_class->finalize = fm_file_search_job_finalize;
+
+	job_class->run = fm_file_search_job_run;
+}
+
+
+static void fm_file_search_job_init(FmFileSearchJob * self)
+{
+    fm_job_init_cancellable(FM_JOB(self));
+}
+
+static void fm_file_search_job_finalize(GObject * object)
+{
+	FmFileSearchJob *self;
+
+	g_return_if_fail(object != NULL);
+	g_return_if_fail(FM_IS_FILE_SEARCH_JOB(object));
+
+	self = FM_FILE_SEARCH_JOB(object);
+
+	if(self->files)
+		fm_list_unref(self->files);
+
+	if(self->target)
+		g_free(self->target);
+		
+	if(self->target_folders)
+		g_slist_free(self->target_folders);
+
+	if(self->target_type)
+		fm_mime_type_unref(self->target_type);
+
+	if (G_OBJECT_CLASS(fm_file_search_job_parent_class)->finalize)
+		(* G_OBJECT_CLASS(fm_file_search_job_parent_class)->finalize)(object);
+}
+
+FmJob * fm_file_search_job_new(FmFileSearch * search)
+{
+	FmFileSearchJob * job = (FmJob*)g_object_new(FM_TYPE_FILE_SEARCH_JOB, NULL);
+
+	job->files = fm_file_info_list_new();
+	job->target = g_strdup(search->target);
+	job->target_folders = g_slist_copy(search->target_folders);
+	job->target_type = fm_mime_type_ref(search->target_type);
+	job->check_type = search->check_type;
+
+	return (FmJob*)job;
+}
+
+gboolean fm_file_search_job_run(FmFileSearchJob * job)
+{
+	/* TODO: error handling (what sort of errors could occur?) */
+
+	g_slist_foreach(job->target_folders, for_each_target_folder, job);
+
+	return TRUE;
+}
+
+static void for_each_target_folder(gpointer data, gpointer user_data)
+{
+	/* TODO: free folder when done */
+	FmFolder * folder = fm_folder_get(FM_PATH(data));
+
+	g_signal_connect(folder, "loaded", on_target_folder_loaded, FM_FILE_SEARCH_JOB(user_data));
+}
+
+static void on_target_folder_loaded(FmFolder * folder, gpointer data)
+{
+	FmFileSearchJob * job = FM_FILE_SEARCH_JOB(data);
+
+	/* TODO: free possible_files when done */
+	FmFileInfoList * possible_files = fm_folder_get_files(folder);
+
+	fm_list_foreach(possible_files, for_each_file_info, job);
+}
+
+static void for_each_file_info(gpointer data, gpointer user_data)
+{
+	
+	/* TODO: handle if the job is canceled since this is called by a for each */
+
+	if(strstr(fm_file_info_get_name(FM_FILE_INFO(data)), FM_FILE_SEARCH_JOB(user_data)->target) != NULL)
+		fm_list_push_tail(FM_FILE_SEARCH_JOB(user_data)->files, FM_FILE_INFO(data)); /* should be referenced because the file list will be freed ? */
+
+	/* recurse upon each directory */
+	if(fm_file_info_is_dir(FM_FILE_INFO(data)))
+		for_each_target_folder(FM_FILE_SEARCH_JOB(user_data), fm_file_info_get_path(FM_FILE_INFO(data)));
+}
+
+FmFileInfoList* fm_file_search_job_get_files(FmFileSearchJob* job)
+{
+	return job->files;
+}
