5725cc9a5dbe69ee11c38da7f8aad376d26d505b
Shae Smittle <starfall87@gmail.com>
2010-06-27 18:19:01 -0500

    work on file search job

--- src/base/fm-file-search.c
+++ src/base/fm-file-search.c
@@ -2,16 +2,16 @@
 
 #include "fm-folder.h"
 
-static void fm_file_search_finalize(GObject * object)
+static void fm_file_search_finalize(GObject * object);
 
 G_DEFINE_TYPE(FmFileSearch, fm_file_search, FM_TYPE_FOLDER);
 
 static void fm_file_search_class_init(FmFileSearchClass *klass)
 {
 	GObjectClass *g_object_class;
-    g_object_class = G_OBJECT_CLASS(klass);
-    g_object_class->finalize = fm_file_search_finalize;
-    fm_file_search_parent_class = (GObjectClass*)g_type_class_peek(FM_TYPE_FOLDER);
+	g_object_class = G_OBJECT_CLASS(klass);
+	g_object_class->finalize = fm_file_search_finalize;
+	fm_file_search_parent_class = (GObjectClass*)g_type_class_peek(FM_TYPE_FOLDER);
 
 	/* TODO: Set up signals here if needed */
 }
@@ -26,13 +26,20 @@ static void fm_file_search_init(FmFileSearch *self)
 
 static void fm_file_search_finalize(GObject * object)
 {
-	FmFileSearch *self;
-    g_return_if_fail(object != NULL);
-    g_return_if_fail(FM_IS_FILE_SEARCH(object));
+	FmFileSearch * self;
+	g_return_if_fail(object != NULL);
+	g_return_if_fail(FM_IS_FILE_SEARCH(object));
 
-    self = FM_FILE_SEARCH(object);
+	self = FM_FILE_SEARCH(object);
 
-	/* TODO: Finalize FmFileSearch here */
+	if(self->target)
+		g_free(self->target);
+		
+	if(self->target_folders)
+		g_slist_free(self->target_folders);
+
+	if(self->target_type)
+		fm_mime_type_unref(self->target_type);
 
 	if (G_OBJECT_CLASS(fm_file_search_parent_class)->finalize)
         (* G_OBJECT_CLASS(fm_file_search_parent_class)->finalize)(object);
---
+++ src/job/fm-file-search-job.h
@@ -0,0 +1,47 @@
+#ifndef __FM_FILE_SEARCH_JOB_H__
+#define __FM_FILE_SEARCH_JOB_H__
+
+#include "fm-job.h"
+#include "fm-file-search.h"
+
+#include "fm-path.h"
+#include "fm-file-info.h"
+
+G_BEGIN_DECLS
+
+#define FM_TYPE_FILE_SEARCH_JOB				(fm_file_search_job_get_type())
+#define FM_FILE_SEARCH_JOB(obj)				(G_TYPE_CHECK_INSTANCE_CAST((obj),\
+			FM_TYPE_FILE_SEARCH_JOB, FmFileSearchJob))
+#define FM_FILE_SEARCH_JOB_CLASS(klass)		(G_TYPE_CHECK_CLASS_CAST((klass),\
+			FM_TYPE_FILE_SEARCH_JOB, FmFileSearchJobClass))
+#define FM_IS_FILE_SEARCH_JOB(obj)			(G_TYPE_CHECK_INSTANCE_TYPE((obj),\
+			FM_TYPE_FILE_SEARCH_JOB))
+#define FM_IS_FILE_SEARCH_JOB_CLASS(klass)	(G_TYPE_CHECK_CLASS_TYPE((klass),\
+			FM_TYPE_FILE_SEARCH_JOB))
+
+typedef struct _FmFileSearchJob			FmFileSearchJob;
+typedef struct _FmFileSearchJobClass		FmFileSearchJobClass;
+
+struct _FmFileSearchJob
+{
+	FmJob parent;
+
+	FmFileInfoList* files;
+	char * target;
+	GSList * target_folders;
+	FmMimeType * target_type;
+	gboolean check_type;
+};
+
+struct _FmFileSearchJobClass
+{
+	FmJobClass parent_class;
+};
+
+GType		fm_file_search_job_get_type		(void);
+FmJob * fm_file_search_job_new(FmFileSearch * search);
+FmFileInfoList* fm_file_search_job_get_files(FmFileSearchJob* job);
+
+G_END_DECLS
+
+#endif
