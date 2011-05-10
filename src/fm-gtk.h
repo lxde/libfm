1d095552970be249adfa86a0c3551e9a25764c85
Hong Jen Yee (PCMan) <pcman.tw@gmail.com>
2011-05-10 23:47:47 +0800

    Include proper headers.

--- src/fm-gtk.h
+++ src/fm-gtk.h
@@ -23,9 +23,15 @@
 #define __FM_GTK_H__
 
 #include "fm.h"
-#include "fm-cell-renderer-text.h"
+#include "fm-app-chooser-combo-box.h"
+#include "fm-app-chooser-dlg.h"
+#include "fm-app-menu-view.h"
 #include "fm-cell-renderer-pixbuf.h"
+#include "fm-cell-renderer-text.h"
 #include "fm-clipboard.h"
+#include "fm-dir-tree-model.h"
+#include "fm-dir-tree-view.h"
+#include "fm-dnd-auto-scroll.h"
 #include "fm-dnd-dest.h"
 #include "fm-dnd-src.h"
 #include "fm-file-menu.h"
@@ -33,16 +39,17 @@
 #include "fm-folder-model.h"
 #include "fm-folder-view.h"
 #include "fm-gtk-file-launcher.h"
+#include "fm-gtk-marshal.h"
 #include "fm-gtk-utils.h"
 #include "fm-icon-pixbuf.h"
+#include "fm-path-bar.h"
 #include "fm-path-entry.h"
+#include "fm-places-model.h"
 #include "fm-places-view.h"
 #include "fm-progress-dlg.h"
-#include "fm-app-chooser-combo-box.h"
-#include "fm-app-chooser-dlg.h"
-#include "fm-app-menu-view.h"
-#include "fm-thumbnail.h"
+#include "fm-side-pane.h"
 #include "fm-tab-label.h"
+#include "fm-thumbnail.h"
 
 G_BEGIN_DECLS
 
