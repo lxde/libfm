5941231678dca005a45be70a98eb2e11fcc3a150
Hong Jen Yee (PCMan) <pcman.tw@gmail.com>
2011-08-11 07:30:53 +0800

    Add warning message for gtk-doc in autogen.sh and add configuration summary in configure.ac.

--- autogen.sh
+++ autogen.sh
@@ -7,7 +7,7 @@ AM_INSTALLED_VERSION=$($AUTOMAKE --version | sed -e '2,$ d' -e 's/.* \([0-9]*\.[
 if [ "$AM_INSTALLED_VERSION" != "1.10" \
     -a "$AM_INSTALLED_VERSION" != "1.11" ];then
     echo
-    echo "You must have automake > 1.10 or 1.11 installed to compile gpicview."
+    echo "You must have automake > 1.10 or 1.11 installed"
     echo "Install the appropriate package for your distribution,"
     echo "or get the source tarball at http://ftp.gnu.org/gnu/automake/"
     exit 1
@@ -24,6 +24,11 @@ else
     echo "http://www.gtk.org/gtk-doc/"
     exit 1
 fi
+
+echo
+echo "If you are going to 'make dist', please add configure option --enable-gtk-doc."
+echo "Otherwise, API documents for libfm won't be correctly built by gtk-doc."
+echo
 
 set -x
 
