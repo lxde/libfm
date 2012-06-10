204692a5c63cd610167fed2a0ac95010124d509e
Hong Jen Yee (PCMan) <pcman.tw@gmail.com>
2012-06-10 17:37:34 +0800

    Little fix for autoge.sh to support automake 1.12.

--- autogen.sh
+++ autogen.sh
@@ -4,14 +4,17 @@ AC_VERSION=
 AUTOMAKE=${AUTOMAKE:-automake}
 AM_INSTALLED_VERSION=$($AUTOMAKE --version | sed -e '2,$ d' -e 's/.* \([0-9]*\.[0-9]*\).*/\1/')
 
+# FIXME: we need a better way for version check later.
 if [ "$AM_INSTALLED_VERSION" != "1.10" \
-    -a "$AM_INSTALLED_VERSION" != "1.11" ];then
-    echo
-    echo "You must have automake > 1.10 or 1.11 installed"
-    echo "Install the appropriate package for your distribution,"
-    echo "or get the source tarball at http://ftp.gnu.org/gnu/automake/"
-    exit 1
+    -a "$AM_INSTALLED_VERSION" != "1.11" \
+    -a "$AM_INSTALLED_VERSION" != "1.12" ];then
+	echo
+	echo "You must have automake > 1.10 or 1.11 installed."
+	echo "Install the appropriate package for your distribution,"
+	echo "or get the source tarball at http://ftp.gnu.org/gnu/automake/"
+	exit 1
 fi
+
 
 if [ "x${ACLOCAL_DIR}" != "x" ]; then
     ACLOCAL_ARG=-I ${ACLOCAL_DIR}
