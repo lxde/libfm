2a0684f3fee9b84353a97987558e61d64edaf600
Andriy Grytsenko <andrej@rep.kiev.ua>
2012-07-12 19:54:06 +0300

    Fixes in developer's documentation.

--- src/base/fm-nav-history.c
+++ src/base/fm-nav-history.c
@@ -19,6 +19,17 @@
  *      MA 02110-1301, USA.
  */
 
+/**
+ * SECTION:fm-nav-history
+ * @short_description: Simple navigation history management.
+ * @title: FmNavHistory
+ *
+ * @include: libfm/fm-nav-history.h
+ *
+ * The #FmNavHistory object implements history for paths that were
+ * entered in some input bar and allows to add, remove or move items in it.
+ */
+
 #include "fm-nav-history.h"
 
 static void fm_nav_history_finalize  			(GObject *object);
@@ -59,33 +70,94 @@ static void fm_nav_history_init(FmNavHistory *self)
 	g_queue_init(&self->items);
 }
 
-
+/**
+ * fm_nav_history_new
+ *
+ * Creates a new #FmNavHistory object with empty history.
+ *
+ * Returns: a new #FmNavHistory object.
+ *
+ * Since: 0.1.0
+ */
 FmNavHistory *fm_nav_history_new(void)
 {
 	return g_object_new(FM_NAV_HISTORY_TYPE, NULL);
 }
 
-/* The returned GList belongs to FmNavHistory and shouldn't be freed. */
+/**
+ * fm_nav_history_list
+ * @nh: the history
+ *
+ * Retrieves full list of the history as #GList of #FmNavHistoryItem.
+ * The returned #GList belongs to #FmNavHistory and shouldn't be freed.
+ *
+ * Returns: (transfer none): full history.
+ *
+ * Since: 0.1.0
+ */
 const GList* fm_nav_history_list(FmNavHistory* nh)
 {
     return nh->items.head;
 }
 
+/**
+ * fm_nav_history_get_cur
+ * @nh: the history
+ *
+ * Retrieves current selected item of the history. The returned item
+ * belongs to #FmNavHistory and shouldn't be freed by caller.
+ *
+ * Returns: (transfer none): current item.
+ *
+ * Since: 0.1.0
+ */
 const FmNavHistoryItem* fm_nav_history_get_cur(FmNavHistory* nh)
 {
     return nh->cur ? (FmNavHistoryItem*)nh->cur->data : NULL;
 }
 
+/**
+ * fm_nav_history_get_cur_link
+ * @nh: the history
+ *
+ * Retrieves current selected item as #GList element containing
+ * #FmNavHistoryItem. The returned item belongs to #FmNavHistory and
+ * shouldn't be freed by caller.
+ *
+ * Returns: (transfer none): current item.
+ *
+ * Since: 0.1.0
+ */
 const GList* fm_nav_history_get_cur_link(FmNavHistory* nh)
 {
     return nh->cur;
 }
 
+/**
+ * fm_nav_history_can_forward
+ * @nh: the history
+ *
+ * Checks if current selected item is the last item in the history.
+ *
+ * Returns: %TRUE if cursor can go forward in history.
+ *
+ * Since: 0.1.0
+ */
 gboolean fm_nav_history_can_forward(FmNavHistory* nh)
 {
     return nh->cur ? (nh->cur->prev != NULL) : FALSE;
 }
 
+/**
+ * fm_nav_history_forward
+ * @nh: the history
+ * @old_scroll_pos: the scroll position to associate with item
+ *
+ * If there is a previous item in the history then marks it current and
+ * sets @old_scroll_pos into it's data.
+ *
+ * Since: 0.1.0
+ */
 void fm_nav_history_forward(FmNavHistory* nh, int old_scroll_pos)
 {
     if(nh->cur && nh->cur->prev)
@@ -97,11 +169,31 @@ void fm_nav_history_forward(FmNavHistory* nh, int old_scroll_pos)
 	}
 }
 
+/**
+ * fm_nav_history_can_back
+ * @nh: the history
+ *
+ * Checks if current selected item is the first item in the history.
+ *
+ * Returns: %TRUE if cursor can go backward in history.
+ *
+ * Since: 0.1.0
+ */
 gboolean fm_nav_history_can_back(FmNavHistory* nh)
 {
     return nh->cur ? (nh->cur->next != NULL) : FALSE;
 }
 
+/**
+ * fm_nav_history_back
+ * @nh: the history
+ * @old_scroll_pos: the scroll position to associate with item
+ *
+ * If there is a next item in the history then marks it current and
+ * sets @old_scroll_pos into it's data.
+ *
+ * Since: 0.1.0
+ */
 void fm_nav_history_back(FmNavHistory* nh, int old_scroll_pos)
 {
 	if(nh->cur && nh->cur->next)
@@ -113,6 +205,18 @@ void fm_nav_history_back(FmNavHistory* nh, int old_scroll_pos)
 	}
 }
 
+/**
+ * fm_nav_history_chdir
+ * @nh: the history
+ * @path: new path to add
+ * @old_scroll_pos: the scroll position to associate with current item
+ *
+ * Adds new @path to the beginning of the @nh and sets @old_scroll_pos
+ * into current item data.
+ *
+ * Since: 0.1.0
+ */
+/* FIXME: it's too dirty, need to redesign it later */
 void fm_nav_history_chdir(FmNavHistory* nh, FmPath* path, int old_scroll_pos)
 {
     FmNavHistoryItem* tmp;
@@ -145,6 +249,17 @@ void fm_nav_history_chdir(FmNavHistory* nh, FmPath* path, int old_scroll_pos)
     }
 }
 
+/**
+ * fm_nav_history_jump
+ * @nh: the history
+ * @l: new current item
+ * @old_scroll_pos: the scroll position to associate with current item
+ *
+ * Sets @old_scroll_pos into current item data and then
+ * sets current item of @nh to one from @l.
+ *
+ * Since: 0.1.0
+ */
 void fm_nav_history_jump(FmNavHistory* nh, GList* l, int old_scroll_pos)
 {
     FmNavHistoryItem* tmp = nh->cur ? (FmNavHistoryItem*)nh->cur->data : NULL;
@@ -154,12 +269,29 @@ void fm_nav_history_jump(FmNavHistory* nh, GList* l, int old_scroll_pos)
     nh->cur = l;
 }
 
+/**
+ * fm_nav_history_clear
+ * @nh: the history
+ *
+ * Removes all items from the history @nh.
+ *
+ * Since: 0.1.0
+ */
 void fm_nav_history_clear(FmNavHistory* nh)
 {
     g_queue_foreach(&nh->items, (GFunc)fm_nav_history_item_free, NULL);
     g_queue_clear(&nh->items);
 }
 
+/**
+ * fm_nav_history_set_max
+ * @nh: the history
+ * @num: new size of history
+ *
+ * Sets maximum length of the history @nh to be @num.
+ *
+ * Since: 0.1.0
+ */
 void fm_nav_history_set_max(FmNavHistory* nh, guint num)
 {
     nh->n_max = num;
