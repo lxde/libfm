0730a52236f919a2cbb3402ff8eb7566b61bb5f5
Andriy Grytsenko <andrej@rep.kiev.ua>
2012-09-14 21:12:51 +0300

    One more little fix on the same subject.

--- src/base/fm-file-info.c
+++ src/base/fm-file-info.c
@@ -278,11 +278,13 @@ void fm_file_info_set_from_gfileinfo(FmFileInfo* fi, GFileInfo* inf)
         case G_FILE_TYPE_UNKNOWN:
             ;
         }
+    }
+
+    if(g_file_info_has_attribute(inf, G_FILE_ATTRIBUTE_ACCESS_CAN_READ))
+        fi->accessible = g_file_info_get_attribute_boolean(inf, G_FILE_ATTRIBUTE_ACCESS_CAN_READ);
+    else
         /* assume it's accessible */
         fi->accessible = TRUE;
-    }
-    else
-        fi->accessible = g_file_info_get_attribute_boolean(inf, G_FILE_ATTRIBUTE_ACCESS_CAN_READ);
 
     /* set "locked" icon on unaccesible folder */
     if(!fi->accessible && type == G_FILE_TYPE_DIRECTORY)
