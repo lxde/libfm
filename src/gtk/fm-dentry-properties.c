84c92e7bec48d40fa07835243a0f9c70c139b3e8
Andriy Grytsenko <andrej@rep.kiev.ua>
2013-05-03 01:55:49 +0300

    Add 3 new settings into third tab of desktop entry properties dialog.
    The commit adds editing of GenericName, Path, and StartupNotify desktop
    entry properties into third tab of desktop entry properties dialog.

--- src/gtk/fm-dentry-properties.c
+++ src/gtk/fm-dentry-properties.c
@@ -38,7 +38,10 @@ struct _FmFilePropertiesDEntryData
     GtkEntry *name;
     GtkEntry *comment;
     GtkEntry *exec;
+    GtkEntry *generic_name;
+    GtkEntry *path;
     GtkToggleButton *terminal;
+    GtkToggleButton *notification;
     gchar *lang;
     gboolean changed;
 };
@@ -94,11 +97,36 @@ static void _dentry_exec_changed(GtkEditable *editable, FmFilePropertiesDEntryDa
     data->changed = TRUE;
 }
 
+static void _dentry_genname_changed(GtkEditable *editable, FmFilePropertiesDEntryData *data)
+{
+    /* g_debug("entry content changed"); */
+    g_key_file_set_string(data->kf, GRP_NAME, "GenericName",
+                          gtk_entry_get_text(GTK_ENTRY(editable)));
+    data->changed = TRUE;
+}
+
+static void _dentry_path_changed(GtkEditable *editable, FmFilePropertiesDEntryData *data)
+{
+    /* g_debug("entry content changed"); */
+    g_key_file_set_string(data->kf, GRP_NAME, "Path",
+                          gtk_entry_get_text(GTK_ENTRY(editable)));
+    data->changed = TRUE;
+}
+
 static void _dentry_terminal_toggled(GtkToggleButton *togglebutton,
                                      FmFilePropertiesDEntryData *data)
 {
     /* g_debug("run in terminal toggled"); */
     g_key_file_set_boolean(data->kf, GRP_NAME, "Terminal",
+                           gtk_toggle_button_get_active(togglebutton));
+    data->changed = TRUE;
+}
+
+static void _dentry_notification_toggled(GtkToggleButton *togglebutton,
+                                         FmFilePropertiesDEntryData *data)
+{
+    /* g_debug("startup notification toggled"); */
+    g_key_file_set_boolean(data->kf, GRP_NAME, "StartupNotify",
                            gtk_toggle_button_get_active(togglebutton));
     data->changed = TRUE;
 }
@@ -239,6 +267,50 @@ static gpointer _dentry_ui_init(GtkBuilder *ui, gpointer uidata, FmFileInfoList 
     g_signal_connect(new_widget, "toggled", G_CALLBACK(_dentry_terminal_toggled), data);
     gtk_table_attach(table, new_widget, 0, 2, 1, 2, GTK_FILL, 0, 18, 0);
     /* FIXME: add checkbox for 'Leave terminal after exit' */
+    /* row 3: "GenericName" GtkEntry */
+    new_widget = gtk_label_new(NULL);
+    label = GTK_LABEL(new_widget);
+    gtk_misc_set_alignment(GTK_MISC(new_widget), 0.0, 0.0);
+    gtk_label_set_markup_with_mnemonic(label, _("<b>D_escription:</b>"));
+    gtk_table_attach(table, new_widget, 0, 1, 3, 4, GTK_FILL, 0, 0, 0);
+    new_widget = gtk_entry_new();
+    data->generic_name = GTK_ENTRY(new_widget);
+    txt = g_key_file_get_locale_string(data->kf, GRP_NAME, "GenericName", NULL, NULL);
+    if (txt)
+        gtk_entry_set_text(data->generic_name, txt);
+    gtk_widget_set_tooltip_text(new_widget, _("Generic name of the application"));
+    g_signal_connect(new_widget, "changed", G_CALLBACK(_dentry_genname_changed), data);
+    gtk_table_attach(table, new_widget, 1, 2, 3, 4, GTK_FILL, 0, 0, 0);
+    gtk_label_set_mnemonic_widget(label, new_widget);
+    /* row 4: "Path" GtkEntry add 'Working directory' entry field */
+    new_widget = gtk_label_new(NULL);
+    label = GTK_LABEL(new_widget);
+    gtk_misc_set_alignment(GTK_MISC(new_widget), 0.0, 0.0);
+    gtk_label_set_markup_with_mnemonic(label, _("<b>_Working directory:</b>"));
+    gtk_table_attach(table, new_widget, 0, 1, 4, 5, GTK_FILL, 0, 0, 0);
+    new_widget = gtk_entry_new();
+    data->path = GTK_ENTRY(new_widget);
+    txt = g_key_file_get_locale_string(data->kf, GRP_NAME, "Path", NULL, NULL);
+    if (txt)
+        gtk_entry_set_text(data->path, txt);
+    gtk_widget_set_tooltip_text(new_widget,
+                                _("The working directory to run the program in"));
+    g_signal_connect(new_widget, "changed", G_CALLBACK(_dentry_path_changed), data);
+    gtk_table_attach(table, new_widget, 1, 2, 4, 5, GTK_FILL, 0, 0, 0);
+    gtk_label_set_mnemonic_widget(label, new_widget);
+    /* TODO: handle "TryExec" field ? */
+    /* row 6: "StartupNotify" GtkCheckButton */
+    new_widget = gtk_check_button_new_with_mnemonic(_("_Use startup notification"));
+    data->notification = GTK_TOGGLE_BUTTON(new_widget);
+    tmp_bool = g_key_file_get_boolean(data->kf, GRP_NAME, "StartupNotify", &err);
+    if (err) /* no such key present */
+    {
+        tmp_bool = FALSE;
+        g_clear_error(&err);
+    }
+    gtk_toggle_button_set_active(data->notification, tmp_bool);
+    g_signal_connect(new_widget, "toggled", G_CALLBACK(_dentry_notification_toggled), data);
+    gtk_table_attach(table, new_widget, 0, 2, 6, 7, GTK_FILL, 0, 0, 0);
     /* put the table into third tab and enable it */
     widget = gtk_builder_get_object(ui, "extra_tab_label");
     gtk_label_set_markup_with_mnemonic(GTK_LABEL(widget), "_Desktop entry");
