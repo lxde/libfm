7e9e79ad22a247f7541978321a7bdd88c7c0a4bf
Vadim Ushakov <igeekless@gmail.com>
2013-05-11 15:52:19 +0800

    Search Dialog: fix "Smaller than" condition.

--- src/gtk/fm-gtk-file-launcher.c
+++ src/gtk/fm-gtk-file-launcher.c
@@ -535,7 +535,7 @@ static gboolean launch_search(FileSearchUI* ui)
             gint unit_index = gtk_combo_box_get_active(ui->smaller_unit_combo);
             /* convert to bytes */
             max_size *= unit_bytes[unit_index];
-            g_string_append_printf(search_uri, "&min_size=%llu", (guint64)max_size);
+            g_string_append_printf(search_uri, "&max_size=%llu", (guint64)max_size);
         }
 
         if(gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(ui->min_mtime_checkbutton)))
