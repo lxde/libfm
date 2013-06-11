21c400d295c51237ae2efa93a2ad0e41b5d3608b
Andriy Grytsenko <andrej@rep.kiev.ua>
2013-06-11 16:22:44 +0300

    Fix over previous commit (a memory leak).

--- src/vfs/fm-vfs-menu.c
+++ src/vfs/fm-vfs-menu.c
@@ -2196,6 +2196,7 @@ static gboolean _add_directory(const char *path, GCancellable *cancellable,
             char *pathtag, *dir;
             GString *str;
             GFile *dir_file;
+            GFileOutputStream *dir_fs;
 
             /* add <NotDeleted/> */
             child = fm_xml_file_item_new(menuTag_NotDeleted);
@@ -2209,9 +2210,11 @@ static gboolean _add_directory(const char *path, GCancellable *cancellable,
             g_string_append(str, ".directory");
             /* touch .directory file, ignore errors */
             dir_file = g_file_new_for_path(str->str);
-            g_file_create(dir_file, G_FILE_CREATE_NONE, cancellable, NULL);
-            /* FIXME: write default contents if created? */
+            dir_fs = g_file_create(dir_file, G_FILE_CREATE_NONE, cancellable, NULL);
             g_object_unref(dir_file);
+            if (dir_fs)
+                /* FIXME: write default contents if created? */
+                g_object_unref(dir_fs);
             child = fm_xml_file_item_new(menuTag_Directory);
             fm_xml_file_item_append_text(child, str->str, str->len, FALSE);
             fm_xml_file_item_append_child(item, child);
