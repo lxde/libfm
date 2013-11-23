2b6525d03c25d566c16a43017d251bbf88185be3
Andriy Grytsenko <andrej@rep.kiev.ua>
2013-11-23 03:37:27 +0200

    [#3615191]Return matched directory on search:// itself.
    If we enter directory then test it for match then we get incorrect
    result which seems as directory lies inside of itself. We should
    do check before entering directory and enter if it doesn't match.

--- src/modules/vfs-search.c
+++ src/modules/vfs-search.c
@@ -252,6 +252,14 @@ static GFileInfo *_fm_vfs_search_enumerator_next_file(GFileEnumerator *enumerato
         file_info = g_file_enumerator_next_file(iter->enu, cancellable, &err);
         if(file_info)
         {
+            /* check if directory itself matches criteria */
+            if(fm_search_job_match_file(enu, file_info, iter->folder_path,
+                                        cancellable, &err))
+            {
+                g_debug("found matched: %s", g_file_info_get_name(file_info));
+                return file_info;
+            }
+
             /* recurse upon each directory */
             if(err == NULL && enu->recursive &&
                g_file_info_get_file_type(file_info) == G_FILE_TYPE_DIRECTORY)
@@ -264,13 +272,6 @@ static GFileInfo *_fm_vfs_search_enumerator_next_file(GFileEnumerator *enumerato
                     fm_search_job_match_folder(enu, file, cancellable, &err);
                     g_object_unref(file);
                 }
-            }
-
-            if(fm_search_job_match_file(enu, file_info, iter->folder_path,
-                                        cancellable, &err))
-            {
-                /* g_debug("found matched: %s", g_file_info_get_name(file_info)); */
-                return file_info;
             }
 
             g_object_unref(file_info);
