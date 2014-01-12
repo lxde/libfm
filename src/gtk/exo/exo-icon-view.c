a5a88880ed716840cb05d74ca962af61c466b645
Gregor Bollerhey <gbsoftware@arcor.de>
2014-01-12 07:24:54 +0100

    Rubberband-bug when executing files.

--- src/gtk/exo/exo-icon-view.c
+++ src/gtk/exo/exo-icon-view.c
@@ -2600,9 +2600,6 @@ exo_icon_view_button_press_event (GtkWidget      *widget,
               exo_icon_view_item_activated (icon_view, path);
               gtk_tree_path_free (path);
               /* bug #3615031: don't start DnD by double click */
-              if (icon_view->priv->selection_mode == GTK_SELECTION_MULTIPLE &&
-                  gtk_widget_get_realized(widget))
-                exo_icon_view_start_rubberbanding (icon_view, event->x, event->y);
             }
         }
 
