lxde/libfm

commit 1b53fe9765e9fdfd56e4ddd91d87b7d04c55650a

Use g_content_type_guess() always with file name, not just with sample.
Some file types may be recognized incorrectly.

src/base/fm-mime-type.c
@@ -2,6 +2,7 @@
  *      fm-mime-type.c
  *
  *      Copyright 2009 - 2012 Hong Jen Yee (PCMan) <pcman.tw@gmail.com>
+ *      Copyright 2012-2014 Andriy Grytsenko (LStranger) <andrej@rep.kiev.ua>
  *
  *      This program is free software; you can redistribute it and/or modify
  *      it under the terms of the GNU General Public License as published by
@@ -187,7 +188,7 @@ FmMimeType* fm_mime_type_from_native_file(const char* file_path,
                 char buf[4096];
                 len = read(fd, buf, MIN(pstat->st_size, 4096));
                 g_free(type);
-                type = g_content_type_guess(NULL, (guchar*)buf, len, &uncertain);
+                type = g_content_type_guess(base_name, (guchar*)buf, len, &uncertain);
             /* #endif */
                 close(fd);
             }

commit 6f9c399d13975f2622403c1ef557202b454b69ca

Fix content type detection broken by commit 1b53fe9765e9fdfd56e4ddd91d87b7d04c55650a
The detection should be done in three phases instead of two:
1) in case if two types might have the same file suffix it will fail
certainity therefore we come to test file contents.
2) if that test is again uncertain it is reasonable to test both basename
and contents this time.

src/base/fm-mime-type.c
@@ -187,10 +187,23 @@ FmMimeType* fm_mime_type_from_native_file(const char* file_path,
             */
                 char buf[4096];
                 len = read(fd, buf, MIN(pstat->st_size, 4096));
+                const char *tmp;
                 g_free(type);
-                type = g_content_type_guess(base_name, (guchar*)buf, len, &uncertain);
-            /* #endif */
                 close(fd);
+                type = g_content_type_guess(NULL, (guchar*)buf, len, &uncertain);
+                if (uncertain)
+                {
+                    g_free(type);
+                    type = g_content_type_guess(base_name, (guchar*)buf, len, &uncertain);
+                }
+                /* bug: improperly named desktop entries are detected as text/plain */
+                if (uncertain && len > 40 && (tmp = memchr(buf, '[', 40)) != NULL &&
+                    strncmp(tmp, "[Desktop Entry]\n", 16) == 0)
+                {
+                    g_free(type);
+                    return fm_mime_type_ref(desktop_entry_type);
+                }
+            /* #endif */
             }
         }
         mime_type = fm_mime_type_from_name(type);