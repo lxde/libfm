lxde/libfm

commit 2854a4ce01f1e2868f2fa2f453eca5fe3fd8339c

Add a setting to open files with a single middle-click
With this setting, a single middle-click has the same effect as a
double-click with the left mouse button.

The single left-click and the single middle-click settings are mutually
exclusive, i.e. both settings cannot be active at the same time.

This uses the middle mouse button differently to pull request #46
by livanh.

src/base/fm-config.c
@@ -136,6 +136,7 @@ static void fm_config_finalize(GObject *object)
 static void fm_config_init(FmConfig *self)
 {
     self->single_click = FM_CONFIG_DEFAULT_SINGLE_CLICK;
+    self->middle_click = FM_CONFIG_DEFAULT_MIDDLE_CLICK;
     self->auto_selection_delay = FM_CONFIG_DEFAULT_AUTO_SELECTION_DELAY;
     self->use_trash = FM_CONFIG_DEFAULT_USE_TRASH;
     self->confirm_del = FM_CONFIG_DEFAULT_CONFIRM_DEL;
@@ -262,6 +263,9 @@ void fm_config_load_from_key_file(FmConfig* cfg, GKeyFile* kf)
 
     fm_key_file_get_bool(kf, "config", "use_trash", &cfg->use_trash);
     fm_key_file_get_bool(kf, "config", "single_click", &cfg->single_click);
+    fm_key_file_get_bool(kf, "config", "middle_click", &cfg->middle_click);
+    if(cfg->single_click && cfg->middle_click)
+        cfg->middle_click = FALSE;
     fm_key_file_get_int(kf, "config", "auto_selection_delay", &cfg->auto_selection_delay);
     fm_key_file_get_bool(kf, "config", "confirm_del", &cfg->confirm_del);
     fm_key_file_get_bool(kf, "config", "confirm_trash", &cfg->confirm_trash);
@@ -482,6 +486,7 @@ void fm_config_save(FmConfig* cfg, const char* name)
                                "# Autogenerated file, don't edit, your changes will be overwritten.\n"
                                "\n[config]\n");
                 _save_config_bool(str, cfg, single_click);
+                _save_config_bool(str, cfg, middle_click);
                 _save_config_bool(str, cfg, use_trash);
                 _save_config_bool(str, cfg, confirm_del);
                 _save_config_bool(str, cfg, confirm_trash);

src/base/fm-config.h
@@ -46,6 +46,7 @@ typedef struct _FmConfig            FmConfig;
 typedef struct _FmConfigClass       FmConfigClass;
 
 #define     FM_CONFIG_DEFAULT_SINGLE_CLICK      FALSE
+#define     FM_CONFIG_DEFAULT_MIDDLE_CLICK      FALSE
 #define     FM_CONFIG_DEFAULT_USE_TRASH         TRUE
 #define     FM_CONFIG_DEFAULT_CONFIRM_DEL       TRUE
 #define     FM_CONFIG_DEFAULT_CONFIRM_TRASH     TRUE
@@ -115,6 +116,7 @@ typedef enum
  * @auto_selection_delay: (since 1.2.0) delay for autoselection in single-click mode, in ms
  * @drop_default_action: (since 1.2.0) default action on drop (see #FmDndDestDropAction)
  * @single_click: single click to open file
+ * @middle_click: (since 1.3.3) middle click to open file
  * @use_trash: delete file to trash can
  * @confirm_del: ask before deleting files
  * @confirm_trash: (since 1.2.0) ask before moving files to trash can
@@ -169,6 +171,7 @@ struct _FmConfig
     gint drop_default_action;
 
     gboolean single_click;
+    gboolean middle_click;
     gboolean use_trash;
     gboolean confirm_del;
     gboolean confirm_trash;

src/gtk/exo/exo-icon-view.c
@@ -2,6 +2,7 @@
  * Copyright (c) 2008       Jannis Pohlmann <jannis@xfce.org>
  * Copyright (c) 2004-2006  os-cillation e.K.
  * Copyright (c) 2002,2004  Anders Carlsson <andersca@gnu.org>
+ * Copyright 2024 Ingo Brückl
  *
  * Written by Benedikt Meurer <benny@xfce.org>.
  *
@@ -128,6 +129,7 @@ enum
   PROP_REORDERABLE,
   PROP_SINGLE_CLICK,
   PROP_SINGLE_CLICK_TIMEOUT,
+  PROP_MIDDLE_CLICK,
   PROP_ENABLE_SEARCH,
   PROP_SEARCH_COLUMN,
   /* For scrollable interface */
@@ -633,6 +635,9 @@ struct _ExoIconViewPrivate
   guint                         single_click_timeout_id;
   guint                         single_click_timeout_state;
 
+  /* Middle-click support */
+  guint                         middle_click : 1;
+
   /* Interactive search support */
   guint                         enable_search : 1;
   guint                         search_imcontext_changed : 1;
@@ -995,6 +1000,21 @@ exo_icon_view_class_init (ExoIconViewClass *klass)
                                                       _("The amount of time after which the item under the mouse cursor will be selected automatically in single click mode"),
                                                       0, G_MAXUINT, 0,
                                                       EXO_PARAM_READWRITE));
+
+  /**
+   * ExoIconView:middle-click:
+   *
+   * Determines whether items can be activated by single middle-clicks.
+   *
+   * Since: 1.3.3
+   **/
+  g_object_class_install_property (gobject_class,
+                                   PROP_MIDDLE_CLICK,
+                                   g_param_spec_boolean ("middle-click",
+                                                         _("Middle Click"),
+                                                         _("Whether the items in the view can be activated with middle clicks"),
+                                                         FALSE,
+                                                         EXO_PARAM_READWRITE));
 
   /**
    * ExoIconView:spacing:
@@ -1424,6 +1444,10 @@ exo_icon_view_get_property (GObject      *object,
       g_value_set_uint (value, priv->single_click_timeout);
       break;
 
+    case PROP_MIDDLE_CLICK:
+      g_value_set_boolean (value, priv->middle_click);
+      break;
+
     case PROP_SPACING:
       g_value_set_int (value, priv->spacing);
       break;
@@ -1519,6 +1543,10 @@ exo_icon_view_set_property (GObject      *object,
 
     case PROP_SINGLE_CLICK_TIMEOUT:
       exo_icon_view_set_single_click_timeout (icon_view, g_value_get_uint (value));
+      break;
+
+    case PROP_MIDDLE_CLICK:
+      exo_icon_view_set_middle_click (icon_view, g_value_get_boolean (value));
       break;
 
     case PROP_SPACING:
@@ -2629,7 +2657,8 @@ exo_icon_view_button_press_event (GtkWidget      *widget,
             exo_icon_view_start_rubberbanding (icon_view, event->x, event->y);
         }
     }
-  else if (event->button == 1 && event->type == GDK_2BUTTON_PRESS)
+  else if ((event->button == 1 && event->type == GDK_2BUTTON_PRESS) ||
+           (icon_view->priv->middle_click && event->button == 2 && event->type == GDK_BUTTON_PRESS))
     {
       /* ignore double-click events in single-click mode */
       if (G_LIKELY (!icon_view->priv->single_click))
@@ -2682,7 +2711,7 @@ exo_icon_view_button_press_event (GtkWidget      *widget,
   /* release reference that was taken above */
   g_object_unref(widget);
 
-  return event->button == 1;
+  return event->button == (icon_view->priv->middle_click ? 2 : 1);
 }
 
 
@@ -8358,6 +8387,59 @@ static void
 exo_icon_view_single_click_timeout_destroy (gpointer user_data)
 {
   EXO_ICON_VIEW (user_data)->priv->single_click_timeout_id = 0;
+}
+
+
+
+/*----------------------*
+ * Middle-click support *
+ *----------------------*/
+
+/**
+ * exo_icon_view_get_middle_click:
+ * @icon_view : a #ExoIconView.
+ *
+ * Returns %TRUE if @icon_view is currently in middle click mode,
+ * else %FALSE will be returned.
+ *
+ * Return value: whether @icon_view is currently in middle click mode.
+ *
+ * Since: 1.3.3
+ **/
+gboolean
+exo_icon_view_get_middle_click (const ExoIconView *icon_view)
+{
+  g_return_val_if_fail (EXO_IS_ICON_VIEW (icon_view), FALSE);
+  return icon_view->priv->middle_click;
+}
+
+
+
+/**
+ * exo_icon_view_set_middle_click:
+ * @icon_view    : a #ExoIconView.
+ * @middle_click : %TRUE for middle click, %FALSE for double click mode.
+ *
+ * If @middle_click is %TRUE, @icon_view will be in middle click mode
+ * afterwards, else @icon_view will be in double click mode.
+ *
+ * Since: 1.3.3
+ **/
+void
+exo_icon_view_set_middle_click (ExoIconView *icon_view,
+                                gboolean     middle_click)
+{
+  g_return_if_fail (EXO_IS_ICON_VIEW (icon_view));
+
+  /* normalize the value */
+  middle_click = !!middle_click;
+
+  /* check if we have a new setting here */
+  if (icon_view->priv->middle_click != middle_click)
+    {
+      icon_view->priv->middle_click = middle_click;
+      g_object_notify (G_OBJECT (icon_view), "middle-click");
+    }
 }
 
 

src/gtk/exo/exo-icon-view.h
@@ -1,6 +1,7 @@
 /*-
  * Copyright (c) 2004-2006  os-cillation e.K.
  * Copyright (c) 2002,2004  Anders Carlsson <andersca@gnu.org>
+ * Copyright 2024 Ingo Brückl
  *
  * Written by Benedikt Meurer <benny@xfce.org>.
  *
@@ -229,6 +230,10 @@ void                  exo_icon_view_set_single_click          (ExoIconView      
 guint                 exo_icon_view_get_single_click_timeout  (const ExoIconView        *icon_view);
 void                  exo_icon_view_set_single_click_timeout  (ExoIconView              *icon_view,
                                                                guint                     single_click_timeout);
+
+gboolean              exo_icon_view_get_middle_click          (const ExoIconView        *icon_view);
+void                  exo_icon_view_set_middle_click          (ExoIconView              *icon_view,
+                                                               gboolean                  middle_click);
 
 void                  exo_icon_view_widget_to_icon_coords     (const ExoIconView        *icon_view,
                                                                gint                      wx,

src/gtk/exo/exo-tree-view.c
@@ -1,5 +1,6 @@
 /*-
  * Copyright (c) 2004-2006 Benedikt Meurer <benny@xfce.org>
+ * Copyright 2024 Ingo Brückl
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
@@ -75,6 +76,7 @@ enum
   PROP_0,
   PROP_SINGLE_CLICK,
   PROP_SINGLE_CLICK_TIMEOUT,
+  PROP_MIDDLE_CLICK,
 };
 
 
@@ -124,6 +126,9 @@ struct _ExoTreeViewPrivate
   guint        single_click_timeout;
   gint         single_click_timeout_id;
   guint        single_click_timeout_state;
+
+  /* middle click mode */
+  guint        middle_click : 1;
 
   /* the path below the pointer or NULL */
   GtkTreePath *hover_path;
@@ -221,6 +226,23 @@ exo_tree_view_class_init (ExoTreeViewClass *klass)
                                                       _("The amount of time after which the item under the mouse cursor will be selected automatically in single click mode"),
                                                       0, G_MAXUINT, 0,
                                                       EXO_PARAM_READWRITE));
+
+  /**
+   * ExoTreeView:middle-click:
+   *
+   * %TRUE to activate items using a middle click instead of a
+   * double click.
+   *
+   * Since: 1.3.3
+   **/
+  g_object_class_install_property (gobject_class,
+                                   PROP_MIDDLE_CLICK,
+                                   g_param_spec_boolean ("middle-click",
+                                                         _("Middle Click"),
+                                                         _("Whether the items in the view can be activated with middle clicks"),
+                                                         FALSE,
+                                                         EXO_PARAM_READWRITE));
+
 }
 
 
@@ -271,6 +293,10 @@ exo_tree_view_get_property (GObject    *object,
       g_value_set_uint (value, exo_tree_view_get_single_click_timeout (tree_view));
       break;
 
+    case PROP_MIDDLE_CLICK:
+      g_value_set_boolean (value, exo_tree_view_get_middle_click (tree_view));
+      break;
+
     default:
       G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
       break;
@@ -295,6 +321,10 @@ exo_tree_view_set_property (GObject      *object,
 
     case PROP_SINGLE_CLICK_TIMEOUT:
       exo_tree_view_set_single_click_timeout (tree_view, g_value_get_uint (value));
+      break;
+
+    case PROP_MIDDLE_CLICK:
+      exo_tree_view_set_middle_click (tree_view, g_value_get_boolean (value));
       break;
 
     default:
@@ -957,6 +987,50 @@ exo_tree_view_set_single_click_timeout (ExoTreeView *tree_view,
       g_object_notify (G_OBJECT (tree_view), "single-click-timeout");
     }
 }
+
+/**
+ * exo_tree_view_get_middle_click:
+ * @tree_view : an #ExoTreeView.
+ *
+ * Returns %TRUE if @tree_view is in middle-click mode, else %FALSE.
+ *
+ * Return value: whether @tree_view is in middle-click mode.
+ *
+ * Since: 1.3.3
+ **/
+gboolean
+exo_tree_view_get_middle_click (const ExoTreeView *tree_view)
+{
+  g_return_val_if_fail (EXO_IS_TREE_VIEW (tree_view), FALSE);
+  return tree_view->priv->middle_click;
+}
+
+
+
+/**
+ * exo_tree_view_set_middle_click:
+ * @tree_view    : an #ExoTreeView.
+ * @middle_click : %TRUE to use middle-click for @tree_view, %FALSE otherwise.
+ *
+ * If @middle_click is %TRUE, @tree_view will use middle-click mode, else
+ * the default double-click mode will be used.
+ *
+ * Since: 1.3.3
+ **/
+void
+exo_tree_view_set_middle_click (ExoTreeView *tree_view,
+                                gboolean     middle_click)
+{
+  g_return_if_fail (EXO_IS_TREE_VIEW (tree_view));
+
+  if (tree_view->priv->middle_click != !!middle_click)
+    {
+      tree_view->priv->middle_click = !!middle_click;
+      g_object_notify (G_OBJECT (tree_view), "middle-click");
+    }
+}
+
+
 
 /* 2008.07.16 added by Hong Jen Yee for PCManFM.
  * If activable column is set, only the specified column can be activated.

src/gtk/exo/exo-tree-view.h
@@ -1,6 +1,7 @@
 /* $Id: exo-tree-view.h 20828 2006-04-09 17:16:48Z benny $ */
 /*-
  * Copyright (c) 2004-2006 Benedikt Meurer <benny@xfce.org>
+ * Copyright 2024 Ingo Brückl
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
@@ -83,6 +84,10 @@ void       exo_tree_view_set_single_click         (ExoTreeView       *tree_view,
 guint      exo_tree_view_get_single_click_timeout (const ExoTreeView *tree_view);
 void       exo_tree_view_set_single_click_timeout (ExoTreeView       *tree_view,
                                                    guint              single_click_timeout);
+
+gboolean   exo_tree_view_get_middle_click         (const ExoTreeView *tree_view);
+void       exo_tree_view_set_middle_click         (ExoTreeView       *tree_view,
+                                                   gboolean           middle_click);
 
 /* 2008.07.16 added by Hong Jen Yee for PCManFM.
  * If activable column is set, only the specified column can be activated.

src/gtk/fm-standard-view.c
@@ -4,6 +4,7 @@
  *      Copyright 2009 - 2012 Hong Jen Yee (PCMan) <pcman.tw@gmail.com>
  *      Copyright 2012-2015 Andriy Grytsenko (LStranger) <andrej@rep.kiev.ua>
  *      Copyright 2013 Mamoru TASAKA <mtasaka@fedoraproject.org>
+ *      Copyright 2024 Ingo Brückl
  *
  *      This program is free software; you can redistribute it and/or modify
  *      it under the terms of the GNU General Public License as published by
@@ -87,6 +88,7 @@ struct _FmStandardView
     /* internal switches */
     void (*set_single_click)(GtkWidget* view, gboolean single_click);
     void (*set_auto_selection_delay)(GtkWidget* view, gint auto_selection_delay);
+    void (*set_middle_click)(GtkWidget* view, gboolean middle_click);
     GtkTreePath* (*get_drop_path)(FmStandardView* fv, gint x, gint y);
     void (*set_drag_dest)(FmStandardView* fv, GtkTreePath* tp);
     void (*select_all)(GtkWidget* view);
@@ -125,6 +127,7 @@ static void on_dnd_src_data_get(FmDndSrc* ds, FmStandardView* fv);
 
 static void on_single_click_changed(FmConfig* cfg, FmStandardView* fv);
 static void on_auto_selection_delay_changed(FmConfig* cfg, FmStandardView* fv);
+static void on_middle_click_changed(FmConfig* cfg, FmStandardView* fv);
 static void on_big_icon_size_changed(FmConfig* cfg, FmStandardView* fv);
 static void on_small_icon_size_changed(FmConfig* cfg, FmStandardView* fv);
 static void on_thumbnail_size_changed(FmConfig* cfg, FmStandardView* fv);
@@ -186,6 +189,12 @@ static void on_auto_selection_delay_changed(FmConfig* cfg, FmStandardView* fv)
         fv->set_auto_selection_delay(fv->view, cfg->auto_selection_delay);
 }
 
+static void on_middle_click_changed(FmConfig* cfg, FmStandardView* fv)
+{
+    if(fv->set_middle_click)
+        fv->set_middle_click(fv->view, cfg->middle_click);
+}
+
 static void on_icon_view_item_activated(ExoIconView* iv, GtkTreePath* path, FmStandardView* fv)
 {
     fm_folder_view_item_clicked(FM_FOLDER_VIEW(fv), path, FM_FV_ACTIVATED);
@@ -205,6 +214,7 @@ static void fm_standard_view_init(FmStandardView *self)
     /* config change notifications */
     g_signal_connect(fm_config, "changed::single_click", G_CALLBACK(on_single_click_changed), self);
     g_signal_connect(fm_config, "changed::auto_selection_delay", G_CALLBACK(on_auto_selection_delay_changed), self);
+    g_signal_connect(fm_config, "changed::middle_click", G_CALLBACK(on_middle_click_changed), self);
 
     /* dnd support */
     self->dnd_src = fm_dnd_src_new(NULL);
@@ -360,6 +370,7 @@ static void fm_standard_view_dispose(GObject *object)
 
     g_signal_handlers_disconnect_by_func(fm_config, on_single_click_changed, object);
     g_signal_handlers_disconnect_by_func(fm_config, on_auto_selection_delay_changed, object);
+    g_signal_handlers_disconnect_by_func(fm_config, on_middle_click_changed, object);
 
     if(self->sel_changed_idle)
     {
@@ -646,6 +657,7 @@ static inline void create_icon_view(FmStandardView* fv, GList* sels)
     exo_icon_view_set_single_click((ExoIconView*)fv->view, fm_config->single_click);
     exo_icon_view_set_single_click_timeout((ExoIconView*)fv->view,
                                            fm_config->auto_selection_delay);
+    exo_icon_view_set_middle_click((ExoIconView*)fv->view, fm_config->middle_click);
 
     for(l = sels;l;l=l->next)
         exo_icon_view_select_path((ExoIconView*)fv->view, l->data);
@@ -1036,6 +1048,7 @@ static inline void create_list_view(FmStandardView* fv, GList* sels)
     exo_tree_view_set_single_click((ExoTreeView*)fv->view, fm_config->single_click);
     exo_tree_view_set_single_click_timeout((ExoTreeView*)fv->view,
                                            fm_config->auto_selection_delay);
+    exo_tree_view_set_middle_click((ExoTreeView*)fv->view, fm_config->middle_click);
 
     ts = gtk_tree_view_get_selection(GTK_TREE_VIEW(fv->view));
     g_signal_connect(fv->view, "row-activated", G_CALLBACK(on_tree_view_row_activated), fv);
@@ -1204,6 +1217,7 @@ void fm_standard_view_set_mode(FmStandardView* fv, FmStandardViewMode mode)
             create_icon_view(fv, sels);
             fv->set_single_click = (void(*)(GtkWidget*,gboolean))exo_icon_view_set_single_click;
             fv->set_auto_selection_delay = (void(*)(GtkWidget*,gint))exo_icon_view_set_single_click_timeout;
+            fv->set_middle_click = (void(*)(GtkWidget*,gboolean))exo_icon_view_set_middle_click;
             fv->get_drop_path = get_drop_path_icon_view;
             fv->set_drag_dest = set_drag_dest_icon_item;
             fv->select_all = (void(*)(GtkWidget*))exo_icon_view_select_all;
@@ -1215,6 +1229,7 @@ void fm_standard_view_set_mode(FmStandardView* fv, FmStandardViewMode mode)
             create_list_view(fv, sels);
             fv->set_single_click = (void(*)(GtkWidget*,gboolean))exo_tree_view_set_single_click;
             fv->set_auto_selection_delay = (void(*)(GtkWidget*,gint))exo_tree_view_set_single_click_timeout;
+            fv->set_middle_click = (void(*)(GtkWidget*,gboolean))exo_tree_view_set_middle_click;
             fv->get_drop_path = get_drop_path_list_view;
             fv->set_drag_dest = set_drag_dest_list_item;
             fv->select_all = select_all_list_view;
@@ -1469,12 +1484,14 @@ static gboolean on_btn_pressed(GtkWidget* view, GdkEventButton* evt, FmStandardV
                             gtk_tree_view_set_cursor(GTK_TREE_VIEW(view), tp, NULL, FALSE);
                         }
                     }
+                    if(fm_config->middle_click && evt->button == 2)
+                      fm_folder_view_item_clicked(FM_FOLDER_VIEW(fv), tp, FM_FV_ACTIVATED);
                 }
               }
             }
         }
 
-        if(evt->button == 2) /* middle click */
+        if(evt->button == 2 && !fm_config->middle_click)  /* middle click */
             type = FM_FV_MIDDLE_CLICK;
         else if(evt->button == 3) /* right click */
             type = FM_FV_CONTEXT_MENU;

commit e91fe2ee51832f82843768757cc02125ad17735d

Restore ABI for libfm.so
The following 2 commit:
https://github.com/lxde/libfm/commit/b0545e0e7bd347dbb8001c153f8bd46718d0c852
https://github.com/lxde/libfm/commit/2854a4ce01f1e2868f2fa2f453eca5fe3fd8339c
broke libfm.so ABI because this changed the size of `struct _FmConfig` .

Restore libfm.so ABI compared to before.
Fixes #104 .

src/base/fm-config.c
@@ -260,10 +260,12 @@ static void _parse_drop_default_action(GKeyFile *kf, gint *action)
 void fm_config_load_from_key_file(FmConfig* cfg, GKeyFile* kf)
 {
     char **strv;
+    gboolean bool_val;
 
     fm_key_file_get_bool(kf, "config", "use_trash", &cfg->use_trash);
     fm_key_file_get_bool(kf, "config", "single_click", &cfg->single_click);
-    fm_key_file_get_bool(kf, "config", "middle_click", &cfg->middle_click);
+    fm_key_file_get_bool(kf, "config", "middle_click", &bool_val);
+    cfg->middle_click = bool_val;
     if(cfg->single_click && cfg->middle_click)
         cfg->middle_click = FALSE;
     fm_key_file_get_int(kf, "config", "auto_selection_delay", &cfg->auto_selection_delay);
@@ -280,7 +282,8 @@ void fm_config_load_from_key_file(FmConfig* cfg, GKeyFile* kf)
     fm_key_file_get_bool(kf, "config", "advanced_mode", &cfg->advanced_mode);
     fm_key_file_get_bool(kf, "config", "si_unit", &cfg->si_unit);
     fm_key_file_get_bool(kf, "config", "force_startup_notify", &cfg->force_startup_notify);
-    fm_key_file_get_bool(kf, "config", "date_iso_8601", &cfg->date_iso_8601);
+    fm_key_file_get_bool(kf, "config", "date_iso_8601", &bool_val);
+    cfg->date_iso_8601 = bool_val;
     fm_key_file_get_bool(kf, "config", "backup_as_hidden", &cfg->backup_as_hidden);
     fm_key_file_get_bool(kf, "config", "no_usb_trash", &cfg->no_usb_trash);
     fm_key_file_get_bool(kf, "config", "no_child_non_expandable", &cfg->no_child_non_expandable);

src/base/fm-config.h
@@ -171,7 +171,6 @@ struct _FmConfig
     gint drop_default_action;
 
     gboolean single_click;
-    gboolean middle_click;
     gboolean use_trash;
     gboolean confirm_del;
     gboolean confirm_trash;
@@ -181,7 +180,6 @@ struct _FmConfig
     gboolean si_unit;
     gboolean advanced_mode;
     gboolean force_startup_notify;
-    gboolean date_iso_8601;
     gboolean backup_as_hidden;
     gboolean no_usb_trash;
     gboolean no_child_non_expandable;
@@ -215,9 +213,9 @@ struct _FmConfig
     gboolean smart_desktop_autodrop;
     gchar *saved_search;
     /*< private >*/
-    gpointer _reserved1; /* reserved space for updates until next ABI */
-    gpointer _reserved2;
-    gpointer _reserved3;
+    gintptr middle_click;
+    gintptr date_iso_8601;
+    gpointer _reserved3; /* reserved space for updates until next ABI */
     gpointer _reserved4;
     gpointer _reserved5;
     gpointer _reserved6;